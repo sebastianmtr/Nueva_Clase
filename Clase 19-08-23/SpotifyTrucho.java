e20695f5612103e9abcaf3790c5ab62f749ebfbd
Sebastian <sm.torres@duocuc.cl>
2023-08-19 10:00:35 -0400

    Nueva Clase

---
+++ Clase 19-08-23/SpotifyTrucho.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package cl.duoc.pgy2121.spotifytrucho;
+
+/**
+ *
+ * @author Cetecom
+ */
+public class SpotifyTrucho {
+ 
+    public static void main(String[] args) {
+        char sexo = 'M';
+            String nombre = "Sebastian";
+        boolean isValid = false;
+            byte bytes;
+        int edad = 22;
+            float decimal = 3.14f;
+            double doble = 3.14d;
+            long numeroLargo = 2312312312312312l;
+            short numeroChico = 34;
+            
+        System.out.println("Nombre: " + nombre);
+        System.out.println("edad:" +edad);
+        System.out.println("Activo" +isValid);
+            
+    }
+}
